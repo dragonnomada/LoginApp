13306410511d2a188ae026a3b171316006cfb46d
dragonnomada <dragonnomada123@gmail.com>
2023-05-23 23:41:26 -0600

    Initial Commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Alan Badillo Salas on 23/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
